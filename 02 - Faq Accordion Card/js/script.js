be710a341cc4ccb98a075f2800bb5a844e35613a
Nujsvart <kampfar@gmail.com>
2021-09-11 15:45:32 +0300

    Refactor

--- 02 - Faq Accordion Card/js/script.js
+++ 02 - Faq Accordion Card/js/script.js
@@ -1,9 +1,18 @@
 "use strict";
-
 const headingEl = document.querySelectorAll(".box__heading");
 const boxEl = document.querySelectorAll(".box");
+const icons = document.querySelectorAll(".icon");
 
-// Event Handlers
+//? Event Handlers
+
+const showDetails = e => {
+  hideDetails();
+  changeIcon(e);
+  const detail = e.target.closest(".box").querySelector(".box__details");
+  const heading = e.target.closest(".box").querySelector(".box__heading");
+  detail.classList.add("box__details--active");
+  heading.classList.add("box__heading--active");
+};
 
 const hideDetails = () => {
   boxEl.forEach(box => {
@@ -11,19 +20,19 @@ const hideDetails = () => {
     if (active) active.classList.remove("box__details--active");
   });
 
-  // remove all active headings
+  //! remove all active headings
   headingEl.forEach(heading =>
     heading.classList.remove("box__heading--active")
   );
 };
 
-const showDetails = e => {
-  hideDetails();
-  const detail = e.target.closest(".box").querySelector(".box__details");
-  const heading = e.target.closest(".box").querySelector(".box__heading");
-  detail.classList.add("box__details--active");
-  heading.classList.add("box__heading--active");
+const changeIcon = e => {
+  //! deactive other icons
+  icons.forEach(icon => icon.classList.remove("icon--active"));
+  //! set active icon
+  const icon = e.target.nextElementSibling;
+  icon.classList.add("icon--active");
 };
 
-// Event Listeners
+//? Event Listeners
 headingEl.forEach(heading => heading.addEventListener("click", showDetails));
